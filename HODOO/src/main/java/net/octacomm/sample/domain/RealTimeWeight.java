f07eed9a978814ccf55c7f54354dce9b2d68b5a6
silverlight2017@gmail.com <1213dlgudwns>
2019-01-11 17:46:39 +0900

    도메인 충돌 해결

--- HODOO/src/main/java/net/octacomm/sample/domain/RealTimeWeight.java
+++ HODOO/src/main/java/net/octacomm/sample/domain/RealTimeWeight.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package net.octacomm.sample.domain;
 
 import lombok.Data;
@@ -27,32 +26,4 @@ public class RealTimeWeight implements Domain{
 	
 	private String createDate;
 
-=======
-package net.octacomm.sample.domain;
-
-import lombok.Data;
-
-@Data
-public class RealTimeWeight implements Domain{
-	
-	public RealTimeWeight() {
-		
-	}
-	
-	public RealTimeWeight(String mac, float value) {
-		this.mac = mac;
-		this.value = value;
-	}
-
-	private int id;
-	
-	private String mac;
-	
-	private float value;
-	
-	private String type;
-	
-	private String createDate;
-
->>>>>>> branch 'master' of https://github.com/ahqlab/HodooServer.git
 }
