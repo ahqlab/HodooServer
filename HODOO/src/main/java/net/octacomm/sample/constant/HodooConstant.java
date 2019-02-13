fb3fb09d3369a651e4fb55ab8641920bb671edf7
SongChic <songchic2@gmail.com>
2019-02-13 17:48:14 +0900

    회원탈퇴시 그룹의 마스터 체크 및 데이터 처리

--- HODOO/src/main/java/net/octacomm/sample/constant/HodooConstant.java
+++ HODOO/src/main/java/net/octacomm/sample/constant/HodooConstant.java
@@ -19,6 +19,14 @@ public class HodooConstant {
 	public static final int JA_CODE = 3;
 	public static final int CH_CODE = 4;
 	
+	public static final int FAIL_CODE = -1;
+	public static final int MEMBER_EXIST = -2;
+	public static final int SUCCESS_CODE = 1;
+	public static final int NOT_GROUP_MASTER = 2;
+	
+	public static final int GROUP_NORMAL_MEMBER = 0;
+	public static final int GROUP_MASTER_MEMBER = 1;
+	
 	public static final String EMAIL_FROM_ADDRESS = "hellomyhodoo@gmail.com";
 	
 	public static final String FCM_APIKEY = "AAAAfhtaYsk:APA91bEgKSbdUUKWISstd-k2uDvzCla8anBmDQhibr114NYN7tfpwTI8QTaqamqZSpPwa2746TVIuUYlVGqGbUIH6oUjHI9zz6pzwDdvMt4yPmw492zfc6sAaJpAmukLO8B4fJngr4D_";
