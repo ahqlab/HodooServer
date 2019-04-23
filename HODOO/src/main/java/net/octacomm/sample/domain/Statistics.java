ff4527a565cdd538a5f55b8c380deb77735f1a52
silverlight2017@gmail.com <1213dlgudwns>
2019-04-23 12:08:08 +0900

    weight 쿼리 변경중.. 그래프 작업중ㄷ

--- HODOO/src/main/java/net/octacomm/sample/domain/Statistics.java
+++ HODOO/src/main/java/net/octacomm/sample/domain/Statistics.java
@@ -6,8 +6,16 @@ import lombok.Data;
 
 @Data
 public class Statistics implements Domain {
+	
+	public Statistics() {
+	}
+	
+    public Statistics(String theWeek, float average) {
+    	this.theWeek = theWeek;
+    	this.average = average;
+	}
 
-    private String theHour;
+	private String theHour;
 
     private String theDay;
 
